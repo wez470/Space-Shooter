146649c51a3626008ad7bd0277861dffb05aa10f
Weston <wez470@gmail.com>
2016-08-21 23:50:19 -0600

    Add shooting.

---
+++ Assets/Scripts/Bullet.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Bullet : MonoBehaviour {
+    public float Speed;
+
+    void Awake() {
+        int bulletLayer = gameObject.layer;
+        Physics2D.IgnoreLayerCollision(bulletLayer, bulletLayer, true);
+    }
+
+    void Update() {
+        GetComponent<Rigidbody2D>().velocity = transform.up * Speed * Time.deltaTime;
+    }
+}
